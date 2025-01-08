e0dc8cf3edd29ed661067567d1167b46251abc9f
Aaron J. Spurlock <2616003+div0ky@users.noreply.github.com>
2025-01-08 11:28:12 -0600

    .

--- app/methods/git.js
+++ app/methods/git.js
@@ -11,7 +11,7 @@ export class GitManager {
    * Checks if there are any uncommitted changes in the Git repository.
    * @returns {boolean} True if there are uncommitted changes, false otherwise.
    */
-  uncommittedChanges() {
+  hasChanges() {
     const status = execCommand("git status --porcelain");
     return status.trim().length > 0;
   }
@@ -66,6 +66,79 @@ export class GitManager {
       return true;
     } catch (error) {
       return false;
+    }
+  }
+
+  push(branch) {
+    try {
+      const has_changes = this.hasChanges();
+      if (has_changes) this.stage();
+
+      const exists = this.remoteExists();
+
+      if (exists) {
+        this.spinner.start(`Force pushing changes to ${branch}...`);
+        execCommand(`git push --force origin ${branch}`);
+      } else {
+        this.spinner.start(`Pushing to new remote for ${branch}...`);
+        execCommand(`git push -u origin ${branch}`);
+      }
+
+      this.spinner.success(`Push to ${branch} is done!`);
+    } catch (error) {
+      this.logger.error(error);
+      this.spinner.error("Uh oh. We failed to push!");
+    }
+  }
+
+  remoteExists(branch) {
+    try {
+      this.spinner.start("Seeing if remote branch exists...");
+      const response = execCommand(`git ls-remote --heads origin ${branch}`).trim();
+      this.spinner.success("Remote branch found!");
+      return response !== "";
+    } catch (error) {
+      this.spinner.error("Uh oh. Something went wrong.");
+    }
+  }
+
+  localExists(branch) {
+    try {
+      execCommand(`git rev-parse --verify ${branch}`);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  stage() {
+    try {
+      this.spinner.start("Staging changes...");
+      execCommand("git add .");
+      this.spinner.success("Staging changes... done!");
+    } catch (error) {
+      this.spinner.error("Staging changes... failed!");
+    }
+  }
+
+  checkoutLocalBranch(branch) {
+    if (this.localExists(branch)) {
+      this.spinner.start(`Checking out local branch '${branch}'...`);
+      execCommand(`git checkout ${branch}`);
+      this.spinner.success(`Checked out local branch '${branch}'`);
+    } else {
+      this.spinner.warn(`Local branch '${branch}' does not exist`);
+    }
+  }
+
+  stash(message = "QuickSilver: Temporary Stash") {
+    try {
+      this.spinner.start("Stashing uncommmitted changes...");
+      execCommand(`git stash push -m "${message}"`);
+      this.spinner.success("Stashing uncommmitted changes... done!");
+      return true;
+    } catch (error) {
+      this.spinner.error("Failed to stash changes!");
     }
   }
 }
